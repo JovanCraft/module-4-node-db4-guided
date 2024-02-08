a55b9970cb37e6a8c5b21b4175e29b6d01971882
jovan <jovancraft@gmail.com>
2024-02-08 07:39:33 -0600

    wip

--- data/migrations/20240208130311_first-migration.js
+++ data/migrations/20240208130311_first-migration.js
@@ -2,14 +2,50 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema
+    .createTable('zoos', table => {
+        table.increments('zoo_id')
+        table.string('zoo_name', 128).notNullable()
+        table.string('zoo_address', 128).unique()
+    })
+    .createTable('species', table => {
+        table.increments('species_id')
+        table.string('species_name', 256).notNullable()
+    })
+    .createTable('animals', table => {
+        table.increments('animal_id')
+        table.string('animal_name', 128).notNullable()
+        table.integer('species_id')
+        .unsigned()
+        .notNullable()
+        .references('species_id')
+        .inTable('species')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+    })
+    .createTable('zoo_animals', table => {
+        table.increments('zoo_animal_id')
+        table.integer('zoo_id')
+        .unsigned()
+        .notNullable()
+        .references('zoo_id')
+        .inTable('zoos')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+    })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+    await knex.schema
+    .dropTableIfExists('zoo_animals')
+    .dropTableIfExists('animals')
+    .dropTableIfExists('species')
+    .dropTableIfExists('zoos')
 };
+
+//in the down function, the tables MUST BE DESTROYED IN THE REVERSE ORDER!
